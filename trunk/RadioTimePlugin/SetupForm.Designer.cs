2dedc4d7d6f551ebbc595297407b99c67ecec96b
yoavain <yoavain@c5bfbcec-d779-7df9-1b34-f4d19a819b24>
2013-09-07 01:41:03 +0000

    More rebranding to 'tunein'. Removed donate button in GUI

--- trunk/RadioTimePlugin/SetupForm.Designer.cs
+++ trunk/RadioTimePlugin/SetupForm.Designer.cs
@@ -28,188 +28,171 @@ namespace RadioTimePlugin
     /// </summary>
     private void InitializeComponent()
     {
-      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SetupForm));
-      this.button1 = new System.Windows.Forms.Button();
-      this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-      this.groupBox2 = new System.Windows.Forms.GroupBox();
-      this.textBox_passwd = new System.Windows.Forms.TextBox();
-      this.textBox_user = new System.Windows.Forms.TextBox();
-      this.label2 = new System.Windows.Forms.Label();
-      this.label1 = new System.Windows.Forms.Label();
-      this.checkBox4 = new System.Windows.Forms.CheckBox();
-      this.textBox_name = new System.Windows.Forms.TextBox();
-      this.label3 = new System.Windows.Forms.Label();
-      this.checkBox5 = new System.Windows.Forms.CheckBox();
-      this.pictureBox1 = new System.Windows.Forms.PictureBox();
-      this.checkBox2 = new System.Windows.Forms.CheckBox();
-      this.cbJumpNowPlaying = new System.Windows.Forms.CheckBox();
-      this.groupBox2.SuspendLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-      this.SuspendLayout();
-      // 
-      // button1
-      // 
-      this.button1.Location = new System.Drawing.Point(250, 209);
-      this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(75, 23);
-      this.button1.TabIndex = 0;
-      this.button1.Text = "Save";
-      this.button1.UseVisualStyleBackColor = true;
-      this.button1.Click += new System.EventHandler(this.button1_Click);
-      // 
-      // linkLabel1
-      // 
-      this.linkLabel1.AutoSize = true;
-      this.linkLabel1.Cursor = System.Windows.Forms.Cursors.Hand;
-      this.linkLabel1.Location = new System.Drawing.Point(6, 235);
-      this.linkLabel1.Name = "linkLabel1";
-      this.linkLabel1.Size = new System.Drawing.Size(287, 13);
-      this.linkLabel1.TabIndex = 4;
-      this.linkLabel1.TabStop = true;
-      this.linkLabel1.Text = "For registering or making presets visit  www.radiotiome.com ";
-      this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
-      // 
-      // groupBox2
-      // 
-      this.groupBox2.Controls.Add(this.textBox_passwd);
-      this.groupBox2.Controls.Add(this.textBox_user);
-      this.groupBox2.Controls.Add(this.label2);
-      this.groupBox2.Controls.Add(this.label1);
-      this.groupBox2.Location = new System.Drawing.Point(3, 2);
-      this.groupBox2.Name = "groupBox2";
-      this.groupBox2.Size = new System.Drawing.Size(183, 101);
-      this.groupBox2.TabIndex = 3;
-      this.groupBox2.TabStop = false;
-      // 
-      // textBox_passwd
-      // 
-      this.textBox_passwd.Location = new System.Drawing.Point(6, 71);
-      this.textBox_passwd.Name = "textBox_passwd";
-      this.textBox_passwd.PasswordChar = '*';
-      this.textBox_passwd.Size = new System.Drawing.Size(171, 20);
-      this.textBox_passwd.TabIndex = 3;
-      // 
-      // textBox_user
-      // 
-      this.textBox_user.Location = new System.Drawing.Point(6, 32);
-      this.textBox_user.Name = "textBox_user";
-      this.textBox_user.Size = new System.Drawing.Size(171, 20);
-      this.textBox_user.TabIndex = 2;
-      // 
-      // label2
-      // 
-      this.label2.AutoSize = true;
-      this.label2.Location = new System.Drawing.Point(6, 55);
-      this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(53, 13);
-      this.label2.TabIndex = 1;
-      this.label2.Text = "Password";
-      // 
-      // label1
-      // 
-      this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(6, 16);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(29, 13);
-      this.label1.TabIndex = 0;
-      this.label1.Text = "User";
-      // 
-      // checkBox4
-      // 
-      this.checkBox4.AutoSize = true;
-      this.checkBox4.Location = new System.Drawing.Point(9, 132);
-      this.checkBox4.Name = "checkBox4";
-      this.checkBox4.Size = new System.Drawing.Size(156, 17);
-      this.checkBox4.TabIndex = 5;
-      this.checkBox4.Text = "Show preset listing on enter";
-      this.checkBox4.UseVisualStyleBackColor = true;
-      // 
-      // textBox_name
-      // 
-      this.textBox_name.Location = new System.Drawing.Point(82, 209);
-      this.textBox_name.Name = "textBox_name";
-      this.textBox_name.Size = new System.Drawing.Size(162, 20);
-      this.textBox_name.TabIndex = 6;
-      // 
-      // label3
-      // 
-      this.label3.AutoSize = true;
-      this.label3.Location = new System.Drawing.Point(9, 214);
-      this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(67, 13);
-      this.label3.TabIndex = 7;
-      this.label3.Text = "Plugin Name";
-      // 
-      // checkBox5
-      // 
-      this.checkBox5.AutoSize = true;
-      this.checkBox5.Location = new System.Drawing.Point(9, 178);
-      this.checkBox5.Name = "checkBox5";
-      this.checkBox5.Size = new System.Drawing.Size(120, 17);
-      this.checkBox5.TabIndex = 8;
-      this.checkBox5.Text = "Use video playback";
-      this.checkBox5.UseVisualStyleBackColor = true;
-      this.checkBox5.Visible = false;
-      // 
-      // pictureBox1
-      // 
-      this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-      this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-      this.pictureBox1.Location = new System.Drawing.Point(192, 12);
-      this.pictureBox1.Name = "pictureBox1";
-      this.pictureBox1.Size = new System.Drawing.Size(131, 91);
-      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-      this.pictureBox1.TabIndex = 9;
-      this.pictureBox1.TabStop = false;
-      this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-      // 
-      // checkBox2
-      // 
-      this.checkBox2.AutoSize = true;
-      this.checkBox2.Location = new System.Drawing.Point(9, 109);
-      this.checkBox2.Name = "checkBox2";
-      this.checkBox2.Size = new System.Drawing.Size(164, 17);
-      this.checkBox2.TabIndex = 0;
-      this.checkBox2.Text = "Start with fast preset window ";
-      this.checkBox2.UseVisualStyleBackColor = true;
-      // 
-      // cbJumpNowPlaying
-      // 
-      this.cbJumpNowPlaying.AutoSize = true;
-      this.cbJumpNowPlaying.Location = new System.Drawing.Point(9, 155);
-      this.cbJumpNowPlaying.Name = "cbJumpNowPlaying";
-      this.cbJumpNowPlaying.Size = new System.Drawing.Size(231, 17);
-      this.cbJumpNowPlaying.TabIndex = 10;
-      this.cbJumpNowPlaying.Text = "Jump to NowPlaying Screen on stream start";
-      this.cbJumpNowPlaying.UseVisualStyleBackColor = true;
-      // 
-      // SetupForm
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(334, 263);
-      this.Controls.Add(this.cbJumpNowPlaying);
-      this.Controls.Add(this.checkBox2);
-      this.Controls.Add(this.pictureBox1);
-      this.Controls.Add(this.checkBox5);
-      this.Controls.Add(this.textBox_name);
-      this.Controls.Add(this.label3);
-      this.Controls.Add(this.checkBox4);
-      this.Controls.Add(this.linkLabel1);
-      this.Controls.Add(this.groupBox2);
-      this.Controls.Add(this.button1);
-      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-      this.MaximizeBox = false;
-      this.MinimizeBox = false;
-      this.Name = "SetupForm";
-      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-      this.Text = "RadioTime Setup";
-      this.Load += new System.EventHandler(this.SetupForm_Load);
-      this.groupBox2.ResumeLayout(false);
-      this.groupBox2.PerformLayout();
-      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-      this.ResumeLayout(false);
-      this.PerformLayout();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox_passwd = new System.Windows.Forms.TextBox();
+            this.textBox_user = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.cbJumpNowPlaying = new System.Windows.Forms.CheckBox();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(250, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.linkLabel1.Location = new System.Drawing.Point(6, 235);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(287, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "For registering or making presets visit  www.radiotiome.com ";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_passwd);
+            this.groupBox2.Controls.Add(this.textBox_user);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(237, 101);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // textBox_passwd
+            // 
+            this.textBox_passwd.Location = new System.Drawing.Point(6, 71);
+            this.textBox_passwd.Name = "textBox_passwd";
+            this.textBox_passwd.PasswordChar = '*';
+            this.textBox_passwd.Size = new System.Drawing.Size(225, 20);
+            this.textBox_passwd.TabIndex = 3;
+            // 
+            // textBox_user
+            // 
+            this.textBox_user.Location = new System.Drawing.Point(6, 32);
+            this.textBox_user.Name = "textBox_user";
+            this.textBox_user.Size = new System.Drawing.Size(225, 20);
+            this.textBox_user.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(9, 132);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(156, 17);
+            this.checkBox4.TabIndex = 5;
+            this.checkBox4.Text = "Show preset listing on enter";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(82, 209);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(162, 20);
+            this.textBox_name.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Plugin Name";
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(9, 178);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(120, 17);
+            this.checkBox5.TabIndex = 8;
+            this.checkBox5.Text = "Use video playback";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.Visible = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(9, 109);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(164, 17);
+            this.checkBox2.TabIndex = 0;
+            this.checkBox2.Text = "Start with fast preset window ";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // cbJumpNowPlaying
+            // 
+            this.cbJumpNowPlaying.AutoSize = true;
+            this.cbJumpNowPlaying.Location = new System.Drawing.Point(9, 155);
+            this.cbJumpNowPlaying.Name = "cbJumpNowPlaying";
+            this.cbJumpNowPlaying.Size = new System.Drawing.Size(231, 17);
+            this.cbJumpNowPlaying.TabIndex = 10;
+            this.cbJumpNowPlaying.Text = "Jump to NowPlaying Screen on stream start";
+            this.cbJumpNowPlaying.UseVisualStyleBackColor = true;
+            // 
+            // SetupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 263);
+            this.Controls.Add(this.cbJumpNowPlaying);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "SetupForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "RadioTime Setup";
+            this.Load += new System.EventHandler(this.SetupForm_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
@@ -226,7 +209,6 @@ namespace RadioTimePlugin
     private System.Windows.Forms.TextBox textBox_name;
     private System.Windows.Forms.Label label3;
     private System.Windows.Forms.CheckBox checkBox5;
-    private System.Windows.Forms.PictureBox pictureBox1;
     private System.Windows.Forms.CheckBox checkBox2;
     private System.Windows.Forms.CheckBox cbJumpNowPlaying;
   }
