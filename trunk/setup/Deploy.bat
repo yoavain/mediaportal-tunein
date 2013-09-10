9ef0b8f7625c6d976c63e0129867aa231b9807eb
yoavain <yoavain@c5bfbcec-d779-7df9-1b34-f4d19a819b24>
2013-09-10 20:10:00 +0000

    Deploy scripts

---
+++ trunk/setup/Deploy.bat
@@ -0,0 +1,17 @@
+@echo off
+cls
+Title Deploying MediaPortal TuneIn (RELEASE)
+cd ..
+
+if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
+	:: 64-bit
+	set PROGS=%programfiles(x86)%
+	goto CONT
+:32BIT
+	set PROGS=%ProgramFiles%	
+:CONT
+
+copy /y "RadioTimePlugin\bin\Release\RadioTimePlugin.dll" "%PROGS%\Team MediaPortal\MediaPortal\plugins\Windows\"
+copy /y "RadioTimePlugin\bin\Release\RadioTimeOpmlApi.dll" "%PROGS%\Team MediaPortal\MediaPortal\plugins\Windows\"
+
+cd setup
---
+++ trunk/setup/Deploy_Debug.bat
@@ -0,0 +1,18 @@
+@echo off
+cls
+Title Deploying MediaPortal TuneIn (DEBUG)
+cd ..
+
+if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
+	:: 64-bit
+	set PROGS=%programfiles(x86)%
+	goto CONT
+:32BIT
+	set PROGS=%ProgramFiles%	
+:CONT
+
+copy /y "RadioTimePlugin\bin\Debug\RadioTimePlugin.dll" "%PROGS%\Team MediaPortal\MediaPortal\plugins\Windows\"
+copy /y "RadioTimePlugin\bin\Debug\RadioTimeOpmlApi.dll" "%PROGS%\Team MediaPortal\MediaPortal\plugins\Windows\"
+
+cd setup
+pause
